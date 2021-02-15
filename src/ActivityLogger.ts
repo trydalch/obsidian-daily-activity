5e023e957def436ea96e336014fbd5fef2f1af78
Trevor Rydalch <trevor.rydalch@outlook.com>
2021-02-15 11:41:28 -0700

    Updated readme and added Today's Stats command

--- src/ActivityLogger.ts
+++ src/ActivityLogger.ts
@@ -1,72 +1,130 @@
-import DailyActivityPlugin from "src/main";
-import { App, getLinkpath, MarkdownView, Plugin } from "obsidian";
+import DailyActivityPlugin from 'src/main';
+import { App, getLinkpath, MarkdownView, Plugin } from 'obsidian';
+import { Moment } from 'moment';
 
 export class ActivityLogger {
+  app: App;
+  plugin: Plugin;
 
-    app: App
-    plugin: Plugin
-    moment: any
+  constructor(app: App, plugin: DailyActivityPlugin) {
+    this.app = app;
+    this.plugin = plugin;
+  }
 
+  private getLinksToFilesModifiedOnDate(moment: Moment) {
+    let files = this.app.vault.getFiles();
+    let links: string[] = [];
+    files.forEach((f) => {
+      if (moment.isSame(new Date(f.stat.mtime), 'day')) {
+        links.push(`[[${getLinkpath(f.path)}]]`);
+      }
+    });
 
-    constructor(app: App, plugin: DailyActivityPlugin) {
-        this.app = app
-        this.plugin = plugin
-    }
+    return links;
+  }
 
-    private getLinksToFilesModifiedToday() {
-        let files = this.app.vault.getFiles()
-        let links: string[] = [];
-        files.forEach(f => {
-            if (this.moment.isSame(new Date(f.stat.mtime), 'day')) {
-                links.push(`[[${getLinkpath(f.path)}]]`);
-            }
-        });
+  private getLinksToFilesCreatedOnDate(moment: Moment) {
+    let files = this.app.vault.getFiles();
+    let links: string[] = [];
+    files.forEach((f) => {
+      if (moment.isSame(new Date(f.stat.ctime), 'day')) {
+        links.push(`[[${getLinkpath(f.path)}]]`);
+      }
+    });
 
-        return links
-    }
+    return links;
+  }
 
-    private getLinksToFilesCreatedToday() {
-        let files = this.app.vault.getFiles()
-        let links: string[] = [];
-        files.forEach(f => {
-            if (this.moment.isSame(new Date(f.stat.ctime), 'day')) {
-                links.push(`[[${getLinkpath(f.path)}]]`);
-            }
-        });
-
-        return links
-    }
-
-    appendLinksToContent(existingContent: string, links: string[], header: string) {
-        return existingContent +  `
+  appendLinksToContent(existingContent: string, links: string[], header: string) {
+    return (
+      existingContent +
+      `
 
 ## ${header} Today: 
 ${links.join('\n')}
 `
+    );
+  }
+
+  async insertActivityLog({
+    insertCreatedToday = false,
+    insertModifiedToday = false,
+    moment = window.moment(),
+    activeView = null,
+  }: {
+    insertCreatedToday?: boolean;
+    insertModifiedToday?: boolean;
+    moment?: any;
+    activeView?: MarkdownView;
+  }) {
+    if (activeView == null) {
+      return;
+    }
+    let editor = activeView.sourceMode.cmEditor;
+    let doc = editor.getDoc();
+
+    let content = await this.app.vault.read(activeView.file);
+    let createdTodayLinks: string[] = [];
+    if (insertCreatedToday) {
+      createdTodayLinks = this.getLinksToFilesCreatedOnDate(moment);
+      content = this.appendLinksToContent(content, createdTodayLinks, 'Created');
+    }
+    if (insertModifiedToday) {
+      let modifiedTodayLinks: string[] = this.getLinksToFilesModifiedOnDate(moment).filter(
+        (link) => createdTodayLinks.indexOf(link) === -1
+      );
+      content = this.appendLinksToContent(content, modifiedTodayLinks, 'Modified');
     }
 
-    async insertActivityLog({insertCreatedToday = false, insertModifiedToday = false}) {
-        this.moment = window.moment();
+    await this.app.vault.modify(activeView.file, content);
+  }
 
-        let activeView = this.app.workspace.getActiveViewOfType(MarkdownView)
-        if (activeView == null) {
-            return;
-        }
-        let editor = activeView.sourceMode.cmEditor;
-        let doc = editor.getDoc();
-
-        let content = await this.app.vault.read(activeView.file)
-        let createdTodayLinks: string[] = []
-        if (insertCreatedToday) {
-            createdTodayLinks = this.getLinksToFilesCreatedToday();
-            content = this.appendLinksToContent(content, createdTodayLinks, 'Created')
-        }
-        if (insertModifiedToday) {
-            let modifiedTodayLinks: string[] = this.getLinksToFilesModifiedToday().filter(link => createdTodayLinks.indexOf(link) === -1);
-            content = this.appendLinksToContent(content, modifiedTodayLinks, 'Modified')
-        }
-        
-        
-        await this.app.vault.modify(activeView.file, content);
+  async insertFileStats({
+    stats = ['created', 'modified'],
+    moment = window.moment(),
+    activeView = null,
+    allTime = false
+  }: {
+    stats?: string[];
+    moment?: any;
+    activeView?: MarkdownView;
+    allTime?: boolean
+  }) {
+    if (activeView == null) {
+      return;
     }
+
+    let content = await this.app.vault.read(activeView.file);
+
+    let header =
+      `| Date |` +
+      stats.join(' | ') +
+      `|
+|-------|${stats.map((s) => '----------').join('|')}|`;
+    let table = header;
+    let row = `|${window.moment(moment).format('YYYY-MM-DD')}|`;
+    stats.forEach((stat) => {
+      let statValue;
+      if (stat == 'created') {
+        statValue = this.getLinksToFilesCreatedOnDate(moment).length;
+      }
+      if (stat == 'modified') {
+        statValue = this.getLinksToFilesModifiedOnDate(moment).length;
+      }
+
+      row = row + `${statValue}|`;
+    });
+
+    table =
+      table +
+      `
+${row}`;
+
+    let newContent =
+      content +
+      `
+
+${table}`;
+    await this.app.vault.modify(activeView.file, newContent);
+  }
 }
